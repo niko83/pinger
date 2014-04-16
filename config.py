9f0e8c96386799df09424e62738c41d475819d11
niko <ktotutus@gmail.com>
2014-04-16 23:14:34 +0300

    fix pep8

--- config.py
+++ config.py
@@ -1,36 +1,34 @@
-#-*- coding: utf-8 -*-
-"""
-Файл с конфигурационными настройками
-"""
+# -*- coding: utf-8 -*-
+u"""Файл с конфигурационными настройками."""
 
 # Configuration
 HOST = 'http://domainForTest.loc'
 HOST_COMPARE_SREEN = 'http://stagging.domainForTest.loc'
 
-#Нужно ли проверять отображение UI
+#: Нужно ли проверять отображение UI
 IS_CHECK_UI = True
 
-#Путь куда сохраняем скриншоты с хостов HOST, HOST_COMPARE_SREEN перед их сравнением
+#: Путь куда сохраняем скриншоты с хостов HOST, HOST_COMPARE_SREEN перед их сравнением
 PATH_TO_SCREENS = '/tmp/'
 
-# absolute or relative path to access nginx log
+#: absolute or relative path to access nginx log
 PATH_TO_NGINX_ACCESS_LOG = '/var/log/nginx/access.log.1'
 
-# Absolute or relative path to directory were is putting log files
+#: Absolute or relative path to directory were is putting log files
 PATH_TO_LOG = 'logs/'
 
-# count latest bites from access log for analize (zero - parsing all file)
+#: count latest bites from access log for analize (zero - parsing all file)
 COUNT_LATEST_BITES = 2 * 1024 * 1024
 
-# count threading
+#: count threading
 COUNT_THREADING = 8
 
-# print current status every PRINT_STATUS_COUNT urls
+#: print current status every PRINT_STATUS_COUNT urls
 PRINT_STATUS_COUNT = 1
 
-# no checked url with substring in logs of string
+#: no checked url with substring in logs of string
 FAKE_SUBSTR = ['POST', 'check', 'Zabbix', '/media/', '/static/']
 
-# credentional for Django sites
+#: credentional for Django sites
 DJANGO_ADMIN_LOGIN = 'dev'
 DJANGO_ADMIN_PASSWORD = 'pool'
